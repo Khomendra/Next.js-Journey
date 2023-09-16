7f10bec1c47c506ec28fd8379f2af083e0df1c15
Khomendra <khomendrakumar.ak.94@gmail.com>
2023-09-16 14:55:52 +0530

    Shows how function works and how data passed in function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,28 @@
-//'use client' tells the browser that all module and component belongs to Client, not Server.
+//
 'use client'
 
 export default function Home() {
+  
+  //1. Decaration of Function inside Home Component
+  const apple = () =>{
+    alert("Apple is a Fruit");
+  }
+
+  //2. How to pass data on a function
+  // const apple = (fruit) =>{
+  //   alert(fruit);
+  // }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <User name="Raj"/>
-      <User name="Peter"/>
-      <button onClick={() => alert("Hello Nextjs")}>Click Me</button>
+      <h1>Event, Function and State</h1>
+
+      {/* 1. shows how can we use function inside COmponent */}
+      <button onClick={() => apple()}>Click Me</button>
+
+      {/* 2. Shows how to pass data on function */}
+      {/* <button onClick={() => apple("Banana")}>Click Me</button> */}
       <h1>Home Page</h1>
     </main>
   )
-}
-
-const User = (prop) =>{
-    return(
-      <div>
-        <h2>My name is {prop.name}</h2>
-      </div>
-    );
 }
